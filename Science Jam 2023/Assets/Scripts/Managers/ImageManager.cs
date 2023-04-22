a5de56febc8651add5b8fbe78c73139d6db391eb
Carl Christensen <carl.christensen98@gmail.com>
2023-04-22 15:27:19 +0200

    Added placeholder art for images, and made the imagemanager function

--- Science Jam 2023/Assets/Scripts/Managers/ImageManager.cs
+++ Science Jam 2023/Assets/Scripts/Managers/ImageManager.cs
@@ -1,11 +1,27 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ImageManager : MonoBehaviour
 {
     public GameObject imagePrefab;
-    public GameObject[] images;
-    
-    
+    public List<GameObject> imageObjects;
+    public Texture2D[] images;
+
+    private void Start()
+    {
+        foreach (var image in images)
+        {
+            GameObject imageObject = Instantiate(imagePrefab);
+            imageObjects.Add(imageObject);
+            imageObject.AddComponent<ImageData>();
+
+            string[] imageData = image.name.Split("_");
+
+            imageObject.GetComponent<ImageData>().type = imageData[0];
+            imageObject.GetComponent<ImageData>().isReal = imageData[1] == "real";
+
+            imageObject.transform.GetChild(1).GetComponent<MeshRenderer>().material.mainTexture = image;
+            imageObject.SetActive(false);
+        }
+    }
 }
